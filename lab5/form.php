dab19206c36f83d6112de392e0e8530b6a17c370
Esther015 <estherlysgnanga@icloud.com>
2026-03-30 10:44:23 +0300

    Update form.php

--- lab5/form.php
+++ lab5/form.php
@@ -65,49 +65,81 @@ if (!empty($messages)) {
     <form action="" method="POST">
 
       <!--name-->
+      <label>ФИО:</label>
       <input name="fio" 
         <?php if ($errors['fio']) {print 'class="error"';} ?> 
         value="<?php print $values['fio']; ?>" 
+        placeholder="Введите ваше ФИО"
       />
 
       <!--phone-->
-      <input name="fio" 
-        <?php if ($errors['fio']) {print 'class="error"';} ?> 
-        value="<?php print $values['fio']; ?>" 
+      <label>Телефон:</label>
+      <input name="phone" 
+        <?php if ($errors['phone']) {print 'class="error"';} ?> 
+        value="<?php print $values['phone']; ?>"
+        placeholder="+7 (999) 123-45-67"
       />
 
       <!--email-->
-      <input name="fio" 
-        <?php if ($errors['fio']) {print 'class="error"';} ?> 
-        value="<?php print $values['fio']; ?>" 
+      <label>Email:</label>
+      <input name="email" 
+        <?php if ($errors['email']) {print 'class="error"';} ?> 
+        value="<?php print $values['email']; ?>" 
+        placeholder="example@mail.com"
       />
 
       <!--date-->
-      <input name="fio" 
-        <?php if ($errors['fio']) {print 'class="error"';} ?> 
-        value="<?php print $values['fio']; ?>" 
+      <label>Дата рождения:</label>
+      <input type="date" name="birthdate" 
+        <?php if ($errors['date']) {print 'class="error"';} ?> 
+        value="<?php print $values['date']; ?>" 
       />
 
       <!--gender-->
-      <input name="fio" 
-        <?php if ($errors['fio']) {print 'class="error"';} ?> 
-        value="<?php print $values['fio']; ?>" 
-      />
+      <label>Пол:</label><br>
+      <input type="radio" name="gender" value="male"
+      <?php if ($values['gender']=='male') print 'checked'; ?>> Мужской
+
+    <input type="radio" name="gender" value="female"
+  <?php if ($values['gender']=='female') print 'checked'; ?>> Женский
+
+<?php if ($errors['gender']) print '<div class="error-message">Ошибка выбора пола</div>'; ?>
+
+<br><br>
 
       <!--languages-->
-      <input name="fio" 
-        <?php if ($errors['fio']) {print 'class="error"';} ?> 
-        value="<?php print $values['fio']; ?>" 
-      />
+     <label>Любимый язык:</label>
+
+<?php
+$selected = [];
+if (!empty($_COOKIE['languages'])) {
+  $selected = json_decode($_COOKIE['languages'], true);
+}
+?>
+
+<select name="languages[]" multiple size="3"
+  <?php if ($errors['languages']) print 'class="error"'; ?>>
+
+<option value="1" <?php if (in_array("1",$selected)) print 'selected'; ?>>Pascal</option>
+<option value="2" <?php if (in_array("2",$selected)) print 'selected'; ?>>C</option>
+<option value="3" <?php if (in_array("3",$selected)) print 'selected'; ?>>C++</option>
+<option value="4" <?php if (in_array("4",$selected)) print 'selected'; ?>>JavaScript</option>
+<option value="5" <?php if (in_array("5",$selected)) print 'selected'; ?>>PHP</option>
+<option value="6" <?php if (in_array("6",$selected)) print 'selected'; ?>>Java</option>
+<option value="7" <?php if (in_array("7",$selected)) print 'selected'; ?>>Python</option>
+
+</select>
 
       <!--biography-->
-      <input name="fio" 
-        <?php if ($errors['fio']) {print 'class="error"';} ?> 
-        value="<?php print $values['fio']; ?>" 
-      />
+      <label>Биография:</label>
+<textarea name="biography"
+  <?php if ($errors['biography']) print 'class="error"'; ?>><?php print $values['biography']; ?></textarea>
 
       <!--contract-->
-      
+      <input type="checkbox" name="contract" value="1"
+  <?php if ($values['contract']) print 'checked'; ?>>
+С контрактом ознакомлен
+
 
       <input type="submit" value="ok" />
     </form>
