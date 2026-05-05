635fd4b8ce4b037b31199eb76ba3c9bdf1a6bc18
Esther015 <estherlysgnanga@icloud.com>
2026-05-05 16:41:25 +0300

    Update index.php

--- lab6/index.php
+++ lab6/index.php
@@ -187,7 +187,7 @@ else {
     }
     
     $selectedLanguages = isset($_POST['languages']) ? $_POST['languages'] : [];
-    $contractValue = isset($_POST['contract']) ? 'yes' : 'no';
+    $contractValue = isset($_POST['contract']) ? $_POST['contract'] : 'no';
     
     try {
         if ($isLoggedIn && $userId) {
