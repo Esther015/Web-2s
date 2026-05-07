159768c381656180da05417cbe831be476152296
Esther015 <estherlysgnanga@icloud.com>
2026-05-07 19:25:33 +0300

    Create index.php

---
+++ Pratique/index.php
@@ -0,0 +1,42 @@
+<?php include 'config.php'; ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Pharmacie</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="container">
+
+    <h1>Système de gestion de pharmacie</h1>
+
+    <div class="menu">
+
+        <a href="medicaments.php" class="card">
+            Médicaments
+        </a>
+
+        <a href="employes.php" class="card">
+            Employés
+        </a>
+
+        <a href="clients.php" class="card">
+            Clients
+        </a>
+
+        <a href="ventes.php" class="card">
+            Ventes
+        </a>
+
+    </div>
+
+</div>
+
+</body>
+</html>
