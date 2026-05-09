c9fb04d7499d64abcd4f6354d058b9130213cb6e
Esther015 <estherlysgnanga@icloud.com>
2026-05-10 01:36:26 +0300

    Update medicines.php

--- Pratique/medicines.php
+++ Pratique/medicines.php
@@ -187,7 +187,7 @@ name="add">
 </button>
 
 </form>
-
+<div class="table-wrapper">
 <table>
 
 <tr>
@@ -282,7 +282,7 @@ href="?delete=<?= $row['id'] ?>">
 <?php } ?>
 
 </table>
-
+</div>
 </div>
 
 </body>
