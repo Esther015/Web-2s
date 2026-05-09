34ed2960f53ade800970b295024b4a03dca3addd
Esther015 <estherlysgnanga@icloud.com>
2026-05-10 01:30:13 +0300

    Update clients.php

--- Pratique/clients.php
+++ Pratique/clients.php
@@ -57,7 +57,7 @@ $result = $pdo->query("SELECT * FROM customers");
 
 <meta charset="UTF-8">
 <title>Клиенты</title>
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="style.css">
 
 </head>
