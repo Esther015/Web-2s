01b118510bb2432674691a53a794e98677171e2d
Esther015 <estherlysgnanga@icloud.com>
2026-05-09 19:41:08 +0300

    Update index.php

--- Pratique/p2/index.php
+++ Pratique/p2/index.php
@@ -1,63 +1,92 @@
+<?php include 'config.php'; ?>
+
 <!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Аптека</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
 
 <div class="layout">
 
-    <!-- SIDEBAR -->
+    <!-- MENU GAUCHE -->
     <div class="sidebar">
 
-        <h2>💊 Аптека</h2>
+        <h2> Аптека</h2>
 
-        <a href="medicines.php">
-            💊 Лекарства
+        <a href="medicines.php" class="menu-link">
+            Лекарства
         </a>
 
-        <a href="employees.php">
-            👨‍⚕️ Сотрудники
+        <a href="employees.php" class="menu-link">
+             Сотрудники
         </a>
 
-        <a href="clients.php">
-            🧑 Клиенты
+        <a href="clients.php" class="menu-link">
+             Клиенты
         </a>
 
-        <a href="sales.php">
-            🛒 Продажи
+        <a href="sales.php" class="menu-link">
+             Продажи
         </a>
 
     </div>
 
 
-    <!-- CONTENU -->
+    <!-- CONTENU DROITE -->
     <div class="content">
 
-        <div class="topbar">
+        <h1>Система управления аптекой</h1>
 
-            <h1>
-                Система управления аптекой
-            </h1>
+        <div class="menu">
+
+            <div class="card">
+                <h2>Лекарства</h2>
+
+                <p>
+                    Gestion des médicaments,
+                    du stock et des dates
+                    d’expiration.
+                </p>
+            </div>
+
+            <div class="card">
+                <h2> Сотрудники</h2>
+
+                <p>
+                    Gestion des employés
+                    et du personnel.
+                </p>
+            </div>
+
+            <div class="card">
+                <h2> Клиенты</h2>
+
+                <p>
+                    Informations des clients
+                    et historique.
+                </p>
+            </div>
+
+            <div class="card">
+                <h2>🛒 Продажи</h2>
+
+                <p>
+                    Gestion des ventes
+                    et du stock.
+                </p>
+            </div>
 
         </div>
 
+    </div>
 
-        <div class="dashboard-cards">
+</div>
 
-            <div class="dashboard-card">
-                <h2>💊</h2>
-                <p>Gestion des médicaments</p>
-            </div>
-
-            <div class="dashboard-card">
-                <h2>👨‍⚕️</h2>
-                <p>Gestion du personnel</p>
-            </div>
-
-            <div class="dashboard-card">
-                <h2>🧑</h2>
-                <p>Gestion des clients</p>
-            </div>
-
-            <div class="dashboard-card">
-                <h2>🛒</h2>
-                <p>Gestion des ventes</p>
-            </div>
-
-        </div>
+</body>
+</html>
