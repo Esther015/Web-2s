0eabdf3877c72d2153ae161516cb04ab11b65523
Esther015 <estherlysgnanga@icloud.com>
2026-05-10 01:31:02 +0300

    Update employees.php

--- Pratique/employees.php
+++ Pratique/employees.php
@@ -67,7 +67,7 @@ $result = $pdo->query("SELECT * FROM employees");
 
 <meta charset="UTF-8">
 <title>Сотрудники</title>
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="style.css">
 
 </head>
