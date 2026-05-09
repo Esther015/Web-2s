6b0caee07548243d330211d1ae1a753374ac9dff
Esther015 <estherlysgnanga@icloud.com>
2026-05-09 19:36:33 +0300

    Create index.php

---
+++ Pratique/p2/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+
+<div class="layout">
+
+    <!-- SIDEBAR -->
+    <div class="sidebar">
+
+        <h2>💊 Аптека</h2>
+
+        <a href="medicines.php">
+            💊 Лекарства
+        </a>
+
+        <a href="employees.php">
+            👨‍⚕️ Сотрудники
+        </a>
+
+        <a href="clients.php">
+            🧑 Клиенты
+        </a>
+
+        <a href="sales.php">
+            🛒 Продажи
+        </a>
+
+    </div>
+
+
+    <!-- CONTENU -->
+    <div class="content">
+
+        <div class="topbar">
+
+            <h1>
+                Система управления аптекой
+            </h1>
+
+        </div>
+
+
+        <div class="dashboard-cards">
+
+            <div class="dashboard-card">
+                <h2>💊</h2>
+                <p>Gestion des médicaments</p>
+            </div>
+
+            <div class="dashboard-card">
+                <h2>👨‍⚕️</h2>
+                <p>Gestion du personnel</p>
+            </div>
+
+            <div class="dashboard-card">
+                <h2>🧑</h2>
+                <p>Gestion des clients</p>
+            </div>
+
+            <div class="dashboard-card">
+                <h2>🛒</h2>
+                <p>Gestion des ventes</p>
+            </div>
+
+        </div>
