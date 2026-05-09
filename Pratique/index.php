be5efaa059792ae6888d9272981f25dd06aea4d1
Esther015 <estherlysgnanga@icloud.com>
2026-05-09 19:43:48 +0300

    Update index.php

--- Pratique/index.php
+++ Pratique/index.php
@@ -12,27 +12,77 @@
 
 <body>
 
-<div class="container">
+<div class="layout">
 
-    <h1>Система управления аптекой</h1>
+    <!-- MENU GAUCHE -->
+    <div class="sidebar">
 
-    <div class="menu">
+        <h2> Аптека</h2>
 
-        <a href="medicines.php" class="card">
+        <a href="medicines.php" class="menu-link">
             Лекарства
         </a>
 
-        <a href="employees.php" class="card">
-            Сотрудники
+        <a href="employees.php" class="menu-link">
+             Сотрудники
         </a>
 
-        <a href="clients.php" class="card">
-            Клиенты
+        <a href="clients.php" class="menu-link">
+             Клиенты
         </a>
 
-        <a href="sales.php" class="card">
-            Продажи
+        <a href="sales.php" class="menu-link">
+             Продажи
         </a>
+
+    </div>
+
+
+    <!-- CONTENU DROITE -->
+    <div class="content">
+
+        <h1>Система управления аптекой</h1>
+
+        <div class="menu">
+
+            <div class="card">
+                <h2>Лекарства</h2>
+
+                <p>
+                    Gestion des médicaments,
+                    du stock et des dates
+                    d’expiration.
+                </p>
+            </div>
+
+            <div class="card">
+                <h2> Сотрудники</h2>
+
+                <p>
+                    Gestion des employés
+                    et du personnel.
+                </p>
+            </div>
+
+            <div class="card">
+                <h2> Клиенты</h2>
+
+                <p>
+                    Informations des clients
+                    et historique.
+                </p>
+            </div>
+
+            <div class="card">
+                <h2>Продажи</h2>
+
+                <p>
+                    Gestion des ventes
+                    et du stock.
+                </p>
+            </div>
+
+        </div>
 
     </div>
 
