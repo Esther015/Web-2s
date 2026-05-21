443ff39e0be2696387ed5d880db9e6d8c35ecfe0
Esther015 <estherlysgnanga@icloud.com>
2026-05-21 21:48:38 +0300

    Update medicines.php

--- Pratique/medicines.php
+++ Pratique/medicines.php
@@ -126,6 +126,14 @@ else {
 <title>Лекарства</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="style.css">
+    <style>
+        h1 {
+    text-align: center;
+    margin-bottom: 30px;
+    margin-top: 0;
+    padding-top: 20px;
+}
+    </style>
 
 </head>
 
